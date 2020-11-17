7ca98fd962d606bb7e3f03655bf14a062101e395
Bohdan Sivak <bohdan.sivak@tcconsulting.it>
2020-11-17 09:10:18 +0100

    twits

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <UserProfile msg = "Hello"/>
+</template>
+
+<script>
+import UserProfile from './components/UserProfile.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    UserProfile
+  }
+}
+</script>
+
+<style>
+html {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  background-color: #F3F5FA;
+}
+
+</style>
