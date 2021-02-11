0bc87d9091e972bf21075829e5a4f81da44d89c2
Bohdan Sivak <bohdan.sivak@tcconsulting.it>
2021-02-11 09:37:34 +0100

    TwitItem and UserProfile

--- src/components/TwitItem.vue
+++ src/components/TwitItem.vue
@@ -1,56 +1,59 @@
 <template>
-    <div class="twit-item">
-        <div class="user-profile__twit">
-            <div class="twit-item__user">
-                @{{username}}
-            </div>
-            <div class="twit-item__contnt">
-                {{twit.content}}
-            </div>
-        </div>     
+  <div class="twit-item" @click="favouriteTwit(twit.id)">
+    <div class="user-profile__twit">
+      <div class="twit-item__user">@{{ username }}</div>
+      <div class="twit-item__contnt">
+        {{ twit.content }}
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "TwitItem",
+  name: "TwitItem",
 
-    props:{
-        username:{
-            type: String,
-            required: true
-        },
-        twit:{
-            type: Object,
-            required: true
-        }
-    }
-    
-}
+  props: {
+    username: {
+      type: String,
+      required: true,
+    },
+    twit: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  methods: {
+    favouriteTwit(id) {
+      this.$emit("favourite", id);
+    },
+  },
+};
 </script>
 
-<style>
-.twit-item{
-    padding: 20px;
-    background-color: white;
-    border-radius: 5px;
-    border: 1px solid #e5e5e5;
-    box-sizing: border-box;
-    cursor: pointer;
-    transition: all 0.25s ease;
-    margin-top: 5px;
-}
+<style lang="scss" scoped>
+.twit-item {
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid #e5e5e5;
+  box-sizing: border-box;
+  cursor: pointer;
+  transition: all 0.25s ease;
+  margin-top: 5px;
 
-.twit-item:hover{
+  &:hover {
     transform: scale(1.1, 1.1);
-}
+  }
 
-.twit-item__user{
+  .twit-item__user {
     padding: auto;
     font-weight: bold;
-}
+  }
 
-.twit-item__contnt{
+  .twit-item__contnt {
     margin-top: 10px;
+  }
 }
 </style>
--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,137 +1,194 @@
 <template>
-<div id="app">
+  <div id="app">
     <div class="user-profile">
       <div class="user-profile__user-panel">
-        <h1 class="user-profile__username">@{{user.username}}</h1>
+        <h1 class="user-profile__username">@{{ user.username }}</h1>
         <div class="user-profile__verificated-badge" v-if="user.isVerificated">
           Verificated
         </div>
         <div class="user-profile_follower-count">
-          <strong>Followers: </strong> {{followers}}
+          <strong>Followers: </strong> {{ followers }}
         </div>
-         <button @click="followMe" id="follow_button" > Follow </button> 
+
+        <button @click="followMe" id="follow_button">Follow</button>
+
+        <form class="user-profile__create-twet" @submit.prevent="createNewTwit">
+          <label for="newTwet"><strong>New Twit</strong></label>
+          <textarea id="newTwit" rows="4" v-model="newTwitContent"></textarea>
+
+          <div class="user-profile__create-twit-type">
+            <label for="newTwitType"><Strong>Type:</Strong></label>
+            <select name="" id="newTwitType" v-model="selectedTwitType">
+              <option
+                :value="option.value"
+                v-for="(option, index) in TwitTypes"
+                :key="index"
+              >
+                {{ option.name }}
+              </option>
+            </select>
+          </div>
+          <button>Twit</button>
+        </form>
       </div>
 
       <div class="user-profile__twits-wrapper">
-        <TwitItem v-for="twit in user.twits" :key="twit.id" :username="user.username" :twit="twit" />
+        <TwitItem
+          v-for="twit in user.twits"
+          :key="twit.id"
+          :username="user.username"
+          :twit="twit"
+          @favourite="toggleFavourite"
+        />
       </div>
-     
     </div>
   </div>
-
-  
 </template>
 
 <script>
-import TwitItem from './TwitItem.vue'
+import TwitItem from "./TwitItem.vue";
 
 export default {
-  name: 'UserProfile',
+  name: "UserProfile",
 
-  components:{
-    TwitItem
+  components: {
+    TwitItem,
   },
 
   props: {
-    msg: String
+    msg: String,
   },
 
-  data(){
-    return{
+  data() {
+    return {
+      newTwitContent: "",
+      selectedTwitType: "instant",
+      TwitTypes: [
+        { value: "draft", name: "Draft" },
+        { value: "instant", name: "Instant Twit" },
+      ],
       followers: 123,
       followed: false,
-      user:{
+      user: {
         id: 1,
-        username: 'n1sb0',
-        firstname: 'Bohdan',
-        lastname: 'Sivak',
-        email: 'bohdan.sivak@gmail.com',
+        username: "n1sb0",
+        firstname: "Bohdan",
+        lastname: "Sivak",
+        email: "bohdan.sivak@gmail.com",
         isVerificated: true,
-        twits:[
-          {id: 1, content: 'This is my first Twet'},
-          {id: 2, content: 'Today is a great day!'},
-          {id: 3, content: 'You!!! how are you :)'},
-          {id: 4, content: 'Awsome post with simple text\'}
-        ]
-      }
-    }
+        twits: [
+          { id: 1, content: "This is my first Twet" },
+          { id: 2, content: "Today is a great day!" },
+          { id: 3, content: "You!!! how are you :)" },
+          { id: 4, content: "Awsome post with simple text" },
+        ],
+      },
+    };
   },
 
-  watch:{
-    followers(newFollowerCount, oldFollowerCount){
-      if(oldFollowerCount < newFollowerCount){
-        console.log(`${this.user.username} has gained a follower!`)
-      }else{
-        console.log(`${this.user.username} losse a follower!`)
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follower!`);
+      } else {
+        console.log(`${this.user.username} losse a follower!`);
       }
-    }
+    },
   },
 
-  methods:{
-    followMe(){
+  methods: {
+    followMe() {
       var btn = document.getElementById("follow_button");
 
-      if (this.followed){
-        this.followers--
-        btn.innerHTML = "Follow"
-      }else{
-        this.followers++
-        btn.innerHTML = "UnFollow"
+      if (this.followed) {
+        this.followers--;
+        btn.innerHTML = "Follow";
+      } else {
+        this.followers++;
+        btn.innerHTML = "UnFollow";
       }
 
-      this.followed = !this.followed
+      this.followed = !this.followed;
     },
 
+    toggleFavourite(id) {
+      console.log(`FAVOURITED Twit #${id}`);
+    },
+
+    createNewTwit() {
+      if (this.newTwitContent && this.selectedTwitType !== "draft") {
+        this.user.twits.unshift({
+          id: this.user.twits.length + 1,
+          content: this.newTwitContent,
+        });
+      }
+      this.newTwitContent = "";
+    },
   },
 
   computed: {
+    newTwitCharacterCount(){
+      return this.newTwitContent.length;
+    },
+
     fullName() {
-      return `${this.user.firstname} ${this.user.lastname}`
-    }
+      return `${this.user.firstname} ${this.user.lastname}`;
+    },
   },
 
-  mounted(){
-    
-  },
-}
+  mounted() {},
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-.user-profile{
+<style lang="scss" scoped>
+.user-profile {
   display: grid;
   grid-template-columns: 1fr 3fr;
   padding: 50px 5%;
+
+  .user-profile__user-panel {
+    display: flex;
+    flex-direction: column;
+    margin-right: 50px;
+    padding: 20px;
+    border-radius: 5px;
+    border: 1px solid #e5e5e5;
+    background-color: white;
+
+    h1 {
+      margin: 0;
+    }
+
+    .user-profile__username {
+      margin-right: auto;
+      margin-bottom: 10px;
+    }
+
+    .user-profile__verificated-badge {
+      background: #42b983;
+      color: white;
+      border-radius: 5px;
+      margin-right: auto;
+      margin-bottom: 5px;
+      padding: 0 10px;
+      font-weight: bold;
+    }
+
+    .user-profile__create-twet {
+      display: flex;
+      flex-direction: column;
+      margin-top: 15px;
+      border-top: 1px solid #333;
+      padding-top: 15px;
+    }
+
+    .user-profile__create-twit-type {
+      margin-top: 15px;
+    }
+  }
 }
 
-.user-profile__verificated-badge{
-  background: #42b983;
-  color: white;
-  border-radius: 5px;
-  margin-right: auto;
-  margin-bottom: 5px;
-  padding: 0 10px;
-  font-weight: bold;
-}
-
-.user-profile__username{
-  margin-right: auto;
-  margin-bottom: 10px;
-}
-
-
-.user-profile__user-panel{
-  display: flex;
-  flex-direction: column;
-  margin-right: 50px;
-  padding: 20px;
-  border-radius: 5px;
-  border: 1px solid #e5e5e5;
-  background-color: white;
-}
-
-button{
+button {
   background: #2c3e50;
   border: 0;
   color: aliceblue;
@@ -143,7 +200,16 @@ button{
   height: 30px;
 }
 
-button:hover{
+button:hover {
   background: #44bba4;
+}
+
+label{
+  color: aliceblue;
+  font-size: 1.2rem;
+  outline-style: none;
+  margin-top: 15px;
+  border-radius: 5px;
+  height: 30px;
 }
 </style>
