1266b5b847435d8b8d0c28d68ea98b2ea531d8de
Bohdan Sivak <bohdan.sivak@tcconsulting.it>
2021-02-11 10:00:17 +0100

    TEST

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -10,7 +10,7 @@
           <strong>Followers: </strong> {{ followers }}
         </div>
 
-        <button @click="followMe" id="follow_button">Follow me!</button>
+        <button @click="followMe" id="follow_button">Follow!!!</button>
 
         <form class="user-profile__create-twet" @submit.prevent="createNewTwit">
           <label for="newTwet"><strong>New Twit</strong></label>
